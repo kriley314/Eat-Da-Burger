8761ce6236c30398ac9c3b8749e362011bf31a78
Ken Riley <kriley314@gmail.com>
2019-04-12 09:42:22 -0500

    Cleaning out some debug prints before jumping into migration to sql heroku support

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,6 @@ var burger = require( "../models/burger.js" );
 
 // Create all our routes and set up logic within those routes where required.
 router.get( "/", function( req, res ) {
-console.log( "Trying to show all burgers.." );
   burger.selectAll( function( data ) {
     var hbsObject = {
       burgers: data
@@ -17,7 +16,6 @@ console.log( "Trying to show all burgers.." );
 });
 
 router.post( "/api/burgers", function( req, res ) {
-  console.log( "Here in router.post for: " + req.body.name );
   // If they didn't actually enter a name, don't do anything..
   if ( req.body.name.length === 0 ) {
     // Go back to the start!!
@@ -34,7 +32,6 @@ router.post( "/api/burgers", function( req, res ) {
 });
 
 router.post( "/api/burgers/:id", function( req, res ) {
-console.log( "Here in router.post with id: " + req.params.id );
   var condition = "id = " + req.params.id;
 
   console.log( "condition: " + condition );
@@ -45,12 +42,6 @@ console.log( "Here in router.post with id: " + req.params.id );
       // Go back to the start!!
       res.redirect( "/" );
   });  
-//    if ( result.changedRows == 0 ) {
-      // If no rows were changed, then the ID must not exist, so 404
-//      return res.status( 404 ).end();
-//    } else {
-//      res.status( 200 ).end();
-//    }
 });
 
 // Export routes for server.js to use.
--- models/burger.js
+++ models/burger.js
@@ -3,20 +3,17 @@ var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function( cb ) {
-console.log( "Here in burger.js selectAll" );
     orm.selectAll( "burgers", function( res ) {
       cb( res );
     });
   },
   // The variables cols and vals are arrays.
   insertOne: function( cols, vals, cb ) {
-console.log( "Here in burger.js insertOne: " + cols + ":" + vals );
         orm.insertOne( "burgers", cols, vals, function( res ) {
       cb( res );
     });
   },
   updateOne: function( objColVals, condition, cb ) {
-console.log( "Here in burger.js updateOne: " + objColVals + ":" + condition );
         orm.updateOne( "burgers", objColVals, condition, function( res ) {
       cb( res );
     });
