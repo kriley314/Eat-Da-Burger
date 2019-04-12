cdda2055940c5c7823cdaf3c2df0361ff28731e9
Ken Riley <kriley314@gmail.com>
2019-04-11 19:26:40 -0500

    Adding partials for the handlebars

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 // Dependencies
 // =============================================================
 var express = require( "express" );
-var path = require( "path" );
+var router = require( "./controllers/burgers_controller.js" );
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use( express.static( "public" ));
+
 // Sets up the Express app to handle data parsing
 app.use( express.urlencoded({ extended: true }));
 app.use( express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
-
+  app.use( router );
 
 // =============================================================================
 // LISTENER
